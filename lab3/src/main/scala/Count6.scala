4da9ea91868ba8d67911d79db74f3f52fa70059c
buhe <bugu1986@126.com>
2022-02-08 14:30:01 +0800

    add some counter

--- lab3/src/main/scala/Count6.scala
+++ lab3/src/main/scala/Count6.scala
@@ -5,11 +5,14 @@ class Count6 extends Module {
     val dout = Output(UInt(8.W))
   })
 
-  val res = Wire(UInt())
+  val res = RegInit(0.U(32.W))
 
   // ***** your code starts here *****
-
-  res := 0.U // dummy code to make it compile
+  // res := 0.U
+  res := res + 1.U // dummy code to make it compile
+  when(res === 6.U) {
+    res := 0.U
+  }
 
   // ***** your code ends here *****
 
